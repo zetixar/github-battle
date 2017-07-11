5e775818d9eee790772f319aa7c031a0d93fd226
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-07-11 08:36:05 -0400

    Add languages selection bar component

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,14 @@
-import React from 'react'
+var React = require('react');
+var Popular = require('./Popular');
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Salam!!!
+      <div className='container'>
+        <Popular />
       </div>
     )
   }
 }
 
-export default App
+module.exports = App;
---
+++ app/components/Popular.js
@@ -0,0 +1,55 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelectLanguage.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+// ----------
+class Popular extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.selectLanguage = this.selectLanguage.bind(this);
+  }
+  selectLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang,
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelectLanguage={this.selectLanguage} />
+      </div>
+    )
+  }
+}
+
+module.exports = Popular;
