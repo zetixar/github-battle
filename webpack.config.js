50063c56a6eb0731c4d2adcd3596dc0116e9ba30
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-07-27 09:53:26 -0400

    Config webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack')
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -17,10 +18,22 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
   },
-  devtool: 'cheap-module-inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
   ]
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  )
+}
+
+module.exports = config
